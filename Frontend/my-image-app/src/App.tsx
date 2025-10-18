13210f78ffdcd29e7ec1932ea7b72ee140461687
Tshireletso Selemela <159533600+tshire-13@users.noreply.github.com>
2025-10-18 13:21:36 +0200

    updates

--- Frontend/my-image-app/src/App.tsx
+++ Frontend/my-image-app/src/App.tsx
@@ -1,10 +1,31 @@
 import { useEffect, useState} from 'react'
 import './App.css'
 import axios from 'axios'
+import React from 'react'
 
 function App() {
 
-const [images, setImages] = useState('');
+  const [images, setImages] = useState('');
+  const [Description, setDescrition] = useState('');
+  const [ImageUrl, setimagesUrl] = useState('');
+  
+  const handleSubmit = async (e)=> {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post(
+        'http://localhost:3000/images',
+        {
+          Description,
+          ImageUrl
+        }
+      ) 
+      alert('POSTED!!!!!!')
+    } catch (error) {
+      console.error('Axios error:', error)
+    }
+
+  }
 
   useEffect(() => {
      
@@ -22,11 +43,30 @@ const [images, setImages] = useState('');
           {images.map((img) => (
             <li key={img.id}>
               <p>{img.Description}</p>
-              <img src={img.ImageUrl} alt={img.Description} width="300" height="300" />
+              <img src={img.ImageUrl} width="300" height="300" />
             </li>
           ))}
         </ul>
       )}
+      <form onSubmit={handleSubmit}>
+
+      <label htmlFor="Description">Enter Description: </label> 
+      <input 
+        value={Description} 
+        id="Description" 
+        name="Description"
+        onChange={(e) => setDescrition(e.target.value)} 
+      /> <br></br> <br></br>
+      <label htmlFor="Description">Enter ImageUrl : </label> 
+      <input 
+        value={ImageUrl} 
+        id="ImageUrl" 
+        name="ImageUrl"
+        onChange={(e) => setimagesUrl(e.target.value)} 
+        /> <br></br> <br></br>
+      <button type="submit">Upload Image</button> 
+
+      </form>
     </div>  
   )
 }
